71035f147cff0fcc4d2d0c0bd08e256d4ad1f43b
Shatice92 <yalcin_htc@hotmail.com>
2025-02-11 16:35:29 +0300

    exception package eklendi

---
+++ src/main/java/org/hatice/ikplus/exception/ErrorType.java
@@ -0,0 +1,21 @@
+package org.hatice.ikplus.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorType {
+	USER_NOT_FOUND(1001, "User not found", HttpStatus.NOT_FOUND),
+	EMPLOYEE_NOT_FOUND(2001, "Employee not found", HttpStatus.NOT_FOUND),
+	USERLIST_EMPTY(1002, "UserList is Empty", HttpStatus.NOT_FOUND),
+	EMPLOYEELIST_EMPTY(2002, "EmployeeList is Empty", HttpStatus.NOT_FOUND),
+	VALIDATION_ERROR(400,"Validation Errors, Please check validation rules",HttpStatus.BAD_REQUEST),
+	INTERNAL_SERVER_ERROR(500,"Server Error, Try Again Later",HttpStatus.INTERNAL_SERVER_ERROR),
+	INVALID_TOKEN(5000,"Invalid Token",HttpStatus.BAD_REQUEST );
+	
+	int code;
+	String message;
+	HttpStatus httpStatus;
+}
